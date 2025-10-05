c46df7d670fc9741b39a54494b854fd464a59098
brnd021 <bernardocustodio686@gmail.com>
2025-10-05 13:48:17 -0300

    correcao da formatacao arquivo js

--- exercicio04.js
+++ exercicio04.js
@@ -20,10 +20,10 @@ let nNumero = isNaN(Math.sqrt(-1));
 let precision = (a+b).toPrecision(1);
 resultado.innerHTML = `Tipo da variavel numero1: ${tipo1} <br>
 Tipo da variavel numero2: ${tipo2} <br>
- Resultado da divisao do numero 1 pelo numero 2: ${divi} <br>
- O valor da raiz quadrada de -1: ${raiz} <br>
- O resultado da soma de a + b: ${soma} <br>
- Formatando o resultado da soma para 2 casas decimais: ${fixed} <br>
- Verificando se a divisao do numero 1 pelo numero 2 e finita: ${finito} <br>
- Verificando se a raiz quadrada de -1 e um numero: ${nNumero} <br>
- Formatando o resultado da soma de a e b para ter um digito valido: ${precision}`;
+Resultado da divisao do numero 1 pelo numero 2: ${divi} <br>
+O valor da raiz quadrada de -1: ${raiz} <br>
+O resultado da soma de a + b: ${soma} <br>
+Formatando o resultado da soma para 2 casas decimais: ${fixed} <br>
+Verificando se a divisao do numero 1 pelo numero 2 e finita: ${finito} <br>
+Verificando se a raiz quadrada de -1 e um numero: ${nNumero} <br>
+Formatando o resultado da soma de a e b para ter um digito valido: ${precision}`;
